6858be626a3d8785de476ca76785e5bccb1b4779
Arnaud Coomans <arnaud.coomans@gmail.com>
2013-02-23 20:56:11 -0800

    Initial Commit

---
+++ Messaging/ACViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACViewController.h
+//  Messaging
+//
+//  Created by Arnaud Coomans on 2/23/13.
+//  Copyright (c) 2013 acoomans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACViewController : UIViewController
+
+@end
---
+++ MessagingTests/MessagingTests.h
@@ -0,0 +1,13 @@
+//
+//  MessagingTests.h
+//  MessagingTests
+//
+//  Created by Arnaud Coomans on 2/23/13.
+//  Copyright (c) 2013 acoomans. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MessagingTests : SenTestCase
+
+@end
